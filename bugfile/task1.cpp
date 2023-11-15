9f88105e54d57d0652e8d1b12cd105d9402fd504
Jerry Zhang <16302010059@fudan.edu.cn>
2023-11-15 17:55:35 +0800

    Update task1.cpp

--- bugfile/task1.cpp
+++ bugfile/task1.cpp
@@ -7,12 +7,24 @@
 using namespace std;
 
 bool has_close_elements(vector<float> numbers, float threshold) {
-    int i, j;
-    for (i = 0; i < numbers.size(); i++) {
-        for (j = i + 1; j < numbers.size(); j++) {
+    for (int i = 0; i < numbers.size(); i++) {
+        for (int j = i + 1; j < numbers.size(); j++) {
             if (abs(numbers[i] - numbers[j]) < threshold)
                 return true;
         }
     }
     return false;
+}
+
+int main() {
+    vector<float> numbers = {1.0, 2.0, 3.5, 4.0};
+    float threshold = 0.5;
+    
+    if (has_close_elements(numbers, threshold)) {
+        printf("There are close elements in the vector.\n");
+    } else {
+        printf("There are no close elements in the vector.\n");
+    }
+
+    return 0;
 }
